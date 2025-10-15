b6f254ed513ad69cf3cd59a1789cb8b4e0850d68
Christober-S <christobersg@gmail.com>
2025-10-15 21:30:21 +0530

    Done Validation

--- src/main/java/chris/springboot/HelloWorld/models/Todo.java
+++ src/main/java/chris/springboot/HelloWorld/models/Todo.java
@@ -3,16 +3,27 @@ package chris.springboot.HelloWorld.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 @Entity
 @Data
 public class Todo {
     @Id
     @GeneratedValue
     Long id;
+
+    @NotNull
+    @NotBlank
     String title;
+
+    @NotNull
+    @NotBlank
     String description;
+
     Boolean isCompleted;
 
+    @Email
+    String email;
 }
